60eef37d2bedf980d5f675c2443d80599594b817
aolmo <aolmoher@uccs.edu>
2019-03-14 22:34:25 -0700

    "solved some strange bug?

--- knn.py
+++ knn.py
@@ -1,15 +1,17 @@
 import random
 import numpy as np
-from io import StringIO
+
+import matplotlib.pyplot as plt
 
 from mnist import MNIST
 
+n_runs = 5
+n_test = 1000
+n_train = 6000
+K = [1, 9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
+
 __RESULTS_DIR__ = "results/"
 mndata = MNIST('data')
-
-
-def display_sample(sample):
-    print(mndata.display(sample))
 
 
 class KNN():
@@ -23,16 +25,16 @@ class KNN():
     def predict(self, k, x):
 
         distances = []
-        argmins = []
+        labels = []
         for x_sample in self.X_train:
             distances.append(self.get_distance(x_sample, x))
 
         for i in range(k):
             argmin_aux = np.argmin(distances)
             distances[argmin_aux] = np.inf
-            argmins.append(self.y_train[argmin_aux])
+            labels.append(self.y_train[argmin_aux])
 
-        return np.bincount(argmins).argmax()
+        return np.bincount(labels).argmax()
 
     def get_accuracy_of_set(self, K, X_set, y_set):
         cnt = 0
@@ -46,26 +48,24 @@ class KNN():
 
         return cnt/n
 
-if __name__ == '__main__':
-    n_runs = 1
-    n_test = 1000
-    n_train = 6000
-    K = [1, 9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
+def display_sample(sample):
+    print(mndata.display(sample))
+
+def train_and_get_error_rates():
 
     X_train, y_train = mndata.load_training()
-    X_test , y_test = mndata.load_testing()
+    X_test, y_test = mndata.load_testing()
 
     X_train, y_train = np.array(X_train[:n_train]), np.array(y_train[:n_train])
     X_test, y_test = np.array(X_test[:n_test]), np.array(y_test[:n_test])
 
-
     knn = KNN(X_train, y_train)
-    train_error_rates = [0]*len(K)
-    test_error_rates = [0]*len(K)
+    train_error_rates = [0] * len(K)
+    test_error_rates = [0] * len(K)
 
     for run in range(n_runs):
         print("==================================")
-        print("[+]: RUN {}".format(run+1))
+        print("[+]: RUN {}".format(run + 1))
         print("==================================")
 
         print("===================")
@@ -76,7 +76,7 @@ if __name__ == '__main__':
             print("[+]: Using K = {}".format(k))
             print("------------------")
             accuracy = knn.get_accuracy_of_set(k, X_test, y_test)
-            test_error_rates[i] += 1-accuracy
+            test_error_rates[i] += (1 - accuracy)
 
         print("===================")
         print("    TRAINING SET   ")
@@ -86,19 +86,29 @@ if __name__ == '__main__':
             print("[+]: Using K = {}".format(k))
             print("------------------")
             accuracy = knn.get_accuracy_of_set(k, X_train, y_train)
-            train_error_rates[i] += 1-accuracy
+            train_error_rates[i] += (1 - accuracy)
 
-        test_error_rates = np.array(test_error_rates)/(run+1)
-        train_error_rates = np.array(train_error_rates)/(run+1)
+    test_error_rates = np.array(test_error_rates) / float(n_runs)
+    train_error_rates = np.array(train_error_rates) / float(n_runs)
 
-        np.save(__RESULTS_DIR__+'test_error_rates_run_{}'.format(run), test_error_rates)
-        np.save(__RESULTS_DIR__+'train_error_rates_run_{}'.format(run), train_error_rates)
+    print("TEST")
+    print(test_error_rates)
 
-    # t1 = np.load('test_error_rates.npy')
-    # t2 = np.load('train_error_rates.npy')
-    #
-    # print("TEST ERROR RATES")
-    # print(t1)
-    # print()
-    # print("TRAIN ERROR RATES")
-    # print(t2)
+    print("TRAIN")
+    print(train_error_rates)
+
+        # np.save(__RESULTS_DIR__ + 'test_error_rates_run_{}'.format(run), test_error_rates)
+        # np.save(__RESULTS_DIR__ + 'train_error_rates_run_{}'.format(run), train_error_rates)
+
+if __name__ == '__main__':
+    # train_and_get_error_rates()
+
+    test_error_rates = np.load(__RESULTS_DIR__+"test_error_rates_run_1.npy")
+    train_error_rates = np.load(__RESULTS_DIR__+"train_error_rates_run_1.npy")
+
+    plt.title("Error rates")
+    plt.plot(K, test_error_rates, 'bo-', label='Test set', linewidth=2)
+    plt.plot(K, train_error_rates, 'ro-', label='Train set', linewidth=2)
+    plt.legend(loc='lower right')
+    plt.show()
+
